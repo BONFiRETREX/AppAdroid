61075d52910b56eab2bff003dbec17e564c28f24
BONFiRETREX <deimardubang@gmail.com>
2026-08-29 18:00:40 -0500

    Agregar pantalla de veterinaria

--- MiPrimeraApp/src/app/index.tsx
+++ MiPrimeraApp/src/app/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Image, Pressable } from 'react-native';
+import { StyleSheet, View, Text, Image, Pressable,Alert } from 'react-native';
 
 export default function HomeScreen() {
   return (
@@ -6,30 +6,36 @@ export default function HomeScreen() {
 
       <Image
         source={{
-          uri: 'https://img.magnific.com/vector-gratis/veterinario-muchos-tipos-animales_1308-65733.jpg?semt=ais_hybrid&w=740&q=80'
+          uri: 'https://png.pngtree.com/png-clipart/20230927/original/pngtree-veterinarian-character-illustration-png-image_13144784.png'
         }}
         style={styles.imagenVeterinaria}
       />
 
       <Text style={styles.titulo}>
-        Desarrollo movil
+        Huellitas Veterinaria
       </Text>
 
       <Text style={styles.texto}>
-        Mi primera app
+         Duban Giron Vt. 
       </Text>
 
       <Text style={styles.texto}>
-        Ing sistemas
+        Cuidamos la salud y bienestar de tus mascotas con atención profesional y mucho cariño.
       </Text>
 
       <Text style={styles.mensaje}>
-        care cui
+        
       </Text>
 
-      <Pressable>
-        <Text>Inicio</Text>
-      </Pressable>
+      <Pressable onPress={() => Alert.alert('Agenda tu cita',
+      'Comunícate con nuestra veterinaria para elegir el día y la hora de atención para tu mascota.')}>
+  <Image
+        source={{
+          uri: 'https://cdn-icons-png.flaticon.com/512/403/403890.png'
+        }}
+        style={styles.imagenBotonInfo}
+      />
+</Pressable>
 
     </View>
   );
@@ -40,26 +46,36 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#DDF3E4', // verde claro pastel
   },
 
   imagenVeterinaria: {
-    width: 300,
-    height: 200,
-    marginTop: 20,
+    width: 412,
+    height: 410,
+    marginTop: -130,
+  },
+  imagenBotonInfo: {
+    width: 100,
+    height: 100,
+    marginTop: 10,
   },
 
   titulo: {
     fontSize: 30,
     fontWeight: 'bold',
+    fontFamily: 'serif',
   },
 
   texto: {
     fontSize: 18,
     marginTop: 10,
+    fontFamily: 'serif',
   },
 
   mensaje: {
     fontSize: 18,
     marginTop: 30,
+    fontFamily: 'serif',
   },
+ 
 });
